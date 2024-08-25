736798b672b7b031a3717c6d728a90839ff0c899
Remi Vansteelandt <remvst@gmail.com>
2024-08-25 13:21:54 -0400

    Make it harder to catch a prisoner

--- src/entities/prisoner.js
+++ src/entities/prisoner.js
@@ -3,10 +3,18 @@ class Prisoner extends Entity {
         super();
         this.buckets.push('human', 'prisoner');
         this.radius = 10;
+
+        this.grabbingLadderRatio = 0;
     }
 
     explode() {
         this.world.remove(this);
+
+        // Just in case, get off the ladder
+        const player = firstItem(this.world.bucket('player'));
+        if (player.hangingPrisoner === this) {
+            player.hangingPrisoner = null;
+        }
     }
 
     cycle(elapsed) {
@@ -27,18 +35,29 @@ class Prisoner extends Entity {
                 if (!obstacle) return;
 
                 this.x += between(
-                    -elapsed * 20,
+                    -elapsed * 10,
                     player.x - this.x,
-                    elapsed * 20,
+                    elapsed * 10,
                 );
                 this.x = between(obstacle.minX, this.x, obstacle.maxX);
                 this.y = obstacle.yAt(this.x) - this.radius;
             }
 
             // Grab the ladder
-            if (abs(player.x - this.x) < 10 && isBetween(player.y, this.y, player.y + player.ladderLength)) {
-                player.hangingPrisoner = this;
-                player.ladderLength = this.y - player.y;
+            if (!player.hangingPrisoner && abs(player.x - this.x) < 20 && isBetween(player.y, this.y, player.y + player.ladderLength)) {
+                this.grabbingLadderRatio += elapsed * 2;
+
+                if (this.grabbingLadderRatio > 1) {
+                    player.hangingPrisoner = this;
+                    player.ladderLength = this.y - player.y;
+
+                    // sound(...[,,618,.01,.13,.11,1,3.1,,2,174,.05,.09,,,,,.58,.27]); // Powerup 216
+                    // sound(...[1.2,,800,.06,.19,.12,,.4,,,361,.06,.08,,,,.05,.52,.27,.18,140]); // Powerup 251
+                    // sound(...[1.2,,258,.05,.28,.28,1,3.2,,,45,.07,.06,,,,.07,.99,.27]); // Powerup 261
+                    sound(...[.6,,370,.01,.01,.13,,.9,40,76,,,,,,,,.91,.03,,925]); // Jump 279
+                }
+            } else {
+                this.grabbingLadderRatio = 0;
             }
 
             // Climb with the ladder
@@ -48,7 +67,11 @@ class Prisoner extends Entity {
 
                 if (dist(player, this) < 20) {
                     player.hangingPrisoner = null;
+                    player.rescuedPrisoners++;
                     this.world.remove(this);
+
+                    // sound(...[1.1,,600,.05,.03,,,4,,,291,.09,,,,,,.79,.01]); // Pickup 373
+                    sound(...[1.2,,529,.06,.19,.12,,.4,,,361,.06,.08,,,,.05,.52,.27,.18,140]); // Loaded Sound 375
                 }
             }
         }

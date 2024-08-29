a821a1e64ba0fc005641b622f0a4c090998d44b5
remvst <remvst@gmail.com>
2024-08-29 01:07:51 -0400

    Animate instructions

--- src/entities/instruction.js
+++ src/entities/instruction.js
@@ -3,16 +3,25 @@ class Instruction extends Entity {
     constructor(instruction) {
         super();
         this.instruction = instruction;
+        this.lastRenderedInstruction = null;
     }
 
     render(camera) {
         ctx.translate(this.x, this.y);
 
-        ctx.globalAlpha = 0.3;
         ctx.fillStyle = '#000';
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        ctx.font = 'bold 48pt Impact';
+        ctx.textAlign = nomangle('center');
+        ctx.textBaseline = nomangle('middle');
+        ctx.font = nomangle('bold 48pt Impact');
+
+        if (this.lastRenderedInstruction !== this.instruction) {
+            this.age = 0;
+            this.lastRenderedInstruction = this.instruction;
+        }
+
+        ctx.globalAlpha = interpolate(0, 0.3, this.age / 0.5);
+        ctx.translate(0, interpolate(-25, 0, this.age / 0.5));
+
         ctx.fillText(this.instruction, 0, 25);
     }
 }

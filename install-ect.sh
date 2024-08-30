a0de6085f524cf18e2075d7ceb1d1ce06632c873
remvst <remvst@gmail.com>
2024-08-30 02:16:57 -0400

    Clone instead of submodule

--- install-ect.sh
+++ install-ect.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+git clone --recursive https://github.com/fhanau/Efficient-Compression-Tool.git
 pushd Efficient-Compression-Tool
-git submodule update --init --recursive
-mkdir -p build
-cd build
-cmake ../src
-make
+    git reset --hard 9aabc2
+    mkdir -p build
+    cd build
+    cmake ../src
+    make
 popd
